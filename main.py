a0221ea09d4739784a83dc36acc64747d1076cee
worksmarter-rp <worksmarter.rp@gmail.com>
2023-02-05 13:43:40 -0500

    More improvements.  No duplicate entries.  auto redirect if not in a game

--- main.py
+++ main.py
@@ -21,15 +21,25 @@ class PlayAgain(FlaskForm):
     submit = SubmitField("Play again?")
 
 #SET DEFAULTS AND ESTABLISH VARIABLES
+game_range = 100
+game_tries = 8
+secret_number = randint(1,100)
+attempts = 0
+game_state = 0
+guesses_over = []
+guesses_under = []
+guesses = []
+stats = {}
 def reset():
-    global game_range, game_tries, secret_number, attempts, state_of_game, guesses_over, guesses_under
+    global game_range, game_tries, secret_number, attempts, game_state, guesses_over, guesses_under, guesses
     game_range = 100
     game_tries = 8
     secret_number = randint(1,100)
     attempts = 0
-    state_of_game = "setup"
+    game_state = 0
     guesses_over = []
     guesses_under = []
+    guesses = []
 reset()
 
 #def var_check():
@@ -37,71 +47,79 @@ reset()
 @app.route('/', methods=["GET","POST"])
 def new_game():
     reset()
-    global game_range, game_tries, secret_number
+    global game_range, game_tries, secret_number, game_state
     form = InputForm()
     #var_check()
     if form.validate_on_submit():
         game_range = form.range.data
         game_tries = form.tries.data
         secret_number = randint(1, game_range)
+        game_state = 1
+
         #var_check()
         return redirect(url_for('play_game'))
     return render_template("index.html", form=form)
+"""
+Game states:
+0 is game not initialized
+1 is game initialized 
+2 is game in play
+3 is attempting the same guess
+4 is game over win
+5 is game over loss
 
+
+"""
 @app.route('/play', methods=["GET","POST"])
 def play_game():
     form = NumberForm()
-    global attempts, state_of_game
-    stats = {
-        "tries": game_tries,
-        "attempts": 0,
-        "guesses": [],
-        "guesses_under": guesses_under,
-        "guesses_over": guesses_over,
-        "secret": secret_number,
-        "game_state": 0,
-    }
-    if form.validate_on_submit():
-        attempts += 1
-        if attempts > game_tries:
-            stats = {
-                "tries": game_tries,
-                "attempts": 0,
-                "guesses": [],
-                "guesses_under": guesses_under,
-                "guesses_over": guesses_over,
-                "secret": secret_number,
-                "game_state": -1,
-            }
-            return render_template('play_game.html', form=form, stats=stats), {"refresh": f'4; url="{url_for("new_game")}"'}
-
-        guess = form.guess.data
-
-
-        game_status = f'you have used {attempts} out of {game_tries}. \n {game_tries-attempts} remain'
-        if guess == secret_number:
-            flash(f"You guessed my number of {secret_number} in {attempts} tries!")
-            game_state = 1
-            direction = "equal"
-        elif guess > secret_number:
-            game_state = 2
-            direction = "smaller"
-            guesses_over.append(guess)
-        elif guess < secret_number:
-            game_state = 3
-            direction = "bigger"
-            guesses_under.append(guess)
+    global attempts, game_state, game_tries, guesses_over, guesses_under, guesses, secret_number, stats
+    if game_state < 1:
+        return redirect(url_for('new_game'))
+    if game_state == 1:
+        game_state = 2
         stats = {
             "tries": game_tries,
             "attempts": attempts,
-            "guesses": [].append(guess),
-            "guesses_under": guesses_under,
-            "guesses_over": guesses_over,
+            "guesses": [],
+            "guesses_under": [],
+            "guesses_over": [],
             "secret": secret_number,
             "game_state": game_state,
-            "direction": direction,
-            "guess":guess
+            "direction": "",
+            "guess": "",
+            "session_hs": ""
         }
+    print(stats)
+
+    if form.validate_on_submit():
+        guess = form.guess.data
+        print(stats)
+        stats["guess"] = form.guess.data
+
+        if guess in stats["guesses"] or guess > game_range:  #not a valid guess
+            stats["game_state"] = 3
+        else:
+            attempts += 1
+            stats["attempts"] = attempts
+            if guess == secret_number:
+                stats["game_state"] = 4
+                return render_template('play_game.html', form=form, stats=stats), {"refresh": f'2; url="{url_for("new_game")}"'}
+            elif attempts == game_tries:
+                stats["game_state"] = 5
+                return render_template('play_game.html', form=form, stats=stats), {
+                    "refresh": f'2; url="{url_for("new_game")}"'}
+            elif guess > secret_number:
+                stats["game_state"] = 2
+                stats["direction"] = "smaller"
+                stats["guesses_over"].append(guess)
+            elif guess < secret_number:
+                stats["game_state"] = 2
+                stats["direction"] = "bigger"
+                stats["guesses_under"].append(guess)
+
+            stats["guesses"].append(guess)
+            print(stats)
         return render_template('play_game.html', form=form, stats=stats)
 
     return render_template('play_game.html', form=form, stats=stats)
